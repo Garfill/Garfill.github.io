da5a3bd3685798db122d7ce392b3ea8a10462e28
Garfill <junhui7guan@163.com>
2018-01-16 11:01:19 +0800

    beauty the nav

--- src/js/cv.js
+++ src/js/cv.js
@@ -6,27 +6,49 @@ function vanish() {
         pages[i].style.display = "none";
     }
 }
-
-function show(num) {
+function show(num1) {
     let pages = document.getElementsByClassName('page');
-    pages[num].style.display = "block";
+    pages[num1].style.display = "block";
+}
+function bt_off() {
+    let navs = document.getElementsByClassName('nav');
+    let len = navs.length;
+    let i;
+    for (i = 0; i < len; i++) {
+        navs[i].style.backgroundColor = 'skyblue';
+    }
+}
+function bt_on(num2){
+    let navs = document.getElementsByClassName('nav');
+    navs[num2].style.backgroundColor = "rgb(179, 177, 177)";
+    navs[num2].style.borderRadius = "20px 20px 0 0";
+}
+function go_p1() {
+    vanish();
+    show(0);
+    bt_off();
+    bt_on(0);
 }
 
-function go_p1() {
-        vanish();
-        show(0);
-}
 function go_p2() {
     vanish();
     show(1);
+    bt_off();
+    bt_on(1);
 }
+
 function go_p3() {
     vanish();
     show(2);
+    bt_off();
+    bt_on(2)
 }
+
 function go_p4() {
     vanish();
     show(3);
+    bt_off();
+    bt_on(3);
 }
 
 
@@ -38,3 +60,4 @@ var n3 = document.getElementById("nav3");
 n3.onclick = go_p3;
 var n4 = document.getElementById("nav4");
 n4.onclick = go_p4;
+
