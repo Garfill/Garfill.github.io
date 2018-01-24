e9749629cfd54c7c7d34bee353a19f3a8bea279c
Garfill <junhui7guan@163.com>
2018-01-24 16:25:03 +0800

    合并了一些函数

--- js/cv.js
+++ js/cv.js
@@ -22,41 +22,19 @@ function bt_on(num2){
     let navs = document.getElementsByClassName('nav');
     navs[num2].style.backgroundColor = "snow";
 }
-function go_p1() {
+function go_page(index) {
     vanish();
-    show(0);
+    show(index);
     bt_off();
-    bt_on(0);
+    bt_on(index);
 }
 
-function go_p2() {
-    vanish();
-    show(1);
-    bt_off();
-    bt_on(1);
-}
-
-function go_p3() {
-    vanish();
-    show(2);
-    bt_off();
-    bt_on(2)
-}
-
-function go_p4() {
-    vanish();
-    show(3);
-    bt_off();
-    bt_on(3);
+for(let i=0;i<5;i++){
+    var navs = document.getElementsByClassName("nav");
+    navs[i].onclick = function(){
+        go_page(i);
+    };
 }
 
 
-var n1 = document.getElementById("nav1");
-n1.onclick = go_p1;
-var n2 = document.getElementById("nav2");
-n2.onclick = go_p2;
-var n3 = document.getElementById("nav3");
-n3.onclick = go_p3;
-var n4 = document.getElementById("nav4");
-n4.onclick = go_p4;
 
