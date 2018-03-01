a77d1dff6669e32600e45e49f5741bea56251184
Garfill <junhui7guan@163.com>
2018-03-01 15:39:36 +0800

    add an animate

--- js/cv.js
+++ js/cv.js
@@ -1,4 +1,4 @@
-function go_page(navs,pages,len,index) {
+function go_page(navs, pages, len, index) {
     //Vanish pages
     for (var i = 0; i < len; i += 1) {
         pages[i].style.display = "none";
@@ -12,18 +12,36 @@ function go_page(navs,pages,len,index) {
     //show button
     navs[index].style.backgroundColor = "snow";
 }
-function start(navs,pages,len) {
+
+function start(navs, pages, len) {
     for (let i = 0; i < len; i += 1) {
         navs[i].onclick = function () {
             console.log(i);
-            go_page(navs,pages,len,i);
+            go_page(navs, pages, len, i);
         };
     }
 }
+
+
 window.onload = function () {
     var navs = document.getElementsByClassName("nav");
     var pages = document.getElementsByClassName("page");
     var len = pages.length;
-    start(navs,pages,len);
-};
+    start(navs, pages, len);
 
+    var index = 0;
+    var str = "Talk is cheap, show the code";
+    var con = document.getElementById("word");
+    var typer = null;
+
+    function type() {
+        con.innerHTML += str[index];
+        index++;
+        if (index == str.length) {
+            clearTimeout(typer);
+        } else {
+            typer = setTimeout(type,100)
+        }
+    }
+    type()
+};
