232a49d3a5eb3ffcb2b526d91438ff3f4cb85db4
Garfill <junhui7guan@163.com>
2022-12-02 15:37:54 +0800

    简单promise

--- code/js/promise/class.js
+++ code/js/promise/class.js
@@ -1,206 +1,39 @@
+/**
+ * 1. class
+ * 2. 代表一个异步操作
+ * 3. 从pending 转换到 funfilled 或者 rejected 
+ * 4. this 作用域问题
+ * 5. 链式调用
+ * 6. 值穿透
+ */
+
 const PENDING = 'pending';
 const FULFILLED = 'fulfilled';
 const REJECTED = 'rejected';
 
-let uid = 0
-
 class MyPromise {
+  constructor(func) {
+    this.PromiseState = PENDING // 状态
+    this.PromiseResult = null // 内部值
 
-  static resolve(param) {
-    if (param instanceof MyPromise) {
-      return param
-    }
-    return new MyPromise((resolve, reject) => {
-      resolve(param)
-    })
+    func(this.resolve.bind(this), this.reject.bind(this)) // 立即执行主体
   }
 
-  static reject(param) {
-    return new MyPromise((resolve, reject) => {
-      reject(param)
-    })
-  }
-
-  static all(promises) {
-    let promiseLength = promises.length
-    let result = new Array(promiseLength)
-    let count = 0
-    return new MyPromise((resolve, reject) => {
-      promises.forEach((item, index) => {
-        MyPromise.resolve(item).then(
-          value => {
-            count++
-            result[index] = value
-            if (count >= promiseLength) {
-              resolve(result)
-            }
-          },
-          reason => {
-            reject(reason)
-          }
-        )
-      })
-    })
-  }
-
-  static race(promises) {
-    return new MyPromise((resolve, reject) => {
-      promises.forEach(item => {
-        MyPromise.resolve(item).then(
-          value => resolve(value),
-          reason => reject(reason)
-        )
-      })
-    })
-  }
-
-  constructor(executor) {
-    this.uid = ++uid;
-
-    this.status = PENDING;
-    this.value = null; // 成功结果
-    this.reason = null; // 失败原因
-    this.fulfilledCb = [];
-    this.rejectedCb = []
-    // 传入handler先执行
-    if (executor && typeof executor === 'function') {
-      try {
-        // 绑定 resolve, reject 上下文
-        // 这样可以传入箭头函数
-        executor(this.resolve.bind(this), this.reject.bind(this))
-      } catch (e) {
-        this.reject(e)
-      }
+  resolve(result) {
+    if (this.PromiseState === PENDING) {
+      this.PromiseState = FULFILLED
+      this.PromiseResult = result
     }
   }
 
-  resolve(value) {
-    if (this.status === PENDING) {
-      this.status = FULFILLED;
-      this.value = value;
-      while (this.fulfilledCb.length) {
-        // 绑定回调函数上下文
-        this.fulfilledCb.shift()(value)
-      }
+  reject(reason) {
+    if (this.PromiseState === PENDING) {
+      this.PromiseState = REJECTED
+      this.PromiseResult = reason
     }
-  }
-
-  reject(error) {
-    if (this.status === PENDING) {
-      this.status = REJECTED;
-      this.reason = error;
-      while (this.rejectedCb.length) {
-        this.rejectedCb.shift()(error)
-      }
-    }
-  }
-
-  then(onFulfilled, onRejected) {
-    onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : value => value;
-    onRejected = typeof onRejected === 'function' ? onRejected : error => { throw error };
-
-    const ret = new MyPromise((resolve, reject) => {    
-      
-      function resolvePromise(result) {
-        if (result === ret) {
-          return reject('self callback')
-        }
-        if (typeof result === 'object' || typeof result === 'function') {
-          if (result === null) return resolve(result)
-
-          let _then;
-          try {
-            _then = result.then
-          } catch (e) {
-            return reject(e)
-          }
-
-          if (typeof _then === 'function') {
-            let called = false;
-            try {
-              _then.call(result, value => {
-                if (called) return
-                called = true
-                resolvePromise(value)
-              }, error => {
-                if (called) return
-                called = true
-                reject(error)
-              })
-            } catch (e) {
-              if (called) return
-              called = true
-              reject(e)
-            }
-          } else {
-            resolve(result)
-          }
-        } else {
-          resolve(result)
-        }
-      }
-
-      if (this.status === FULFILLED) {
-        queueMicrotask(() => {
-          try {
-            const result = onFulfilled(this.value)
-            resolvePromise(result)
-          } catch (e) {
-            reject(e)
-          }
-        })
-      } else if (this.status === REJECTED) {
-        queueMicrotask(() => {
-          try {
-            const result = onRejected(this.reason)
-            resolvePromise(result)
-          } catch (e) {
-            reject(e)
-          }
-        })
-      } else if (this.status === PENDING) {
-
-        this.fulfilledCb.push(() => {
-          queueMicrotask(() => {
-            try {
-              const result = onFulfilled(this.value)
-              resolvePromise(result)
-            } catch (e) {
-              reject(e)
-            }
-          })
-        })
-
-        this.rejectedCb.push(() => {
-          queueMicrotask(() => {
-            try {
-              const result = onRejected(this.reason)
-              resolvePromise(result)
-            } catch (e) {
-              reject(e)
-            }
-          })
-        })
-
-      }
-    });
-    return ret
-  }
-
-  catch(onRejected) {
-    return this.then(null, onRejected)
   }
 }
 
-
-
-let p = MyPromise.resolve('success')
-let p2 = p.then(val => {
-  console.log(val)
-  return {
-    test: 'test'
-  }
+let p0 = new MyPromise((resolve, reject) => {
+  resolve('ok') // 运行时确定this，这里this = undifined，需要在promise内部绑定 this
 })
-setTimeout(() => {
-  console.log(p2)
-}, 0);
