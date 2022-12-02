55863eea86cd9a717abdc6c5e6d3481666263eb4
Garfill <junhui7guan@163.com>
2022-12-02 17:22:47 +0800

    实现then

--- code/js/promise/class.js
+++ code/js/promise/class.js
@@ -1,6 +1,6 @@
 /**
  * 1. class
- * 2. 代表一个异步操作
+ * 2. 异步操作
  * 3. 从pending 转换到 funfilled 或者 rejected 
  * 4. this 作用域问题
  * 5. 链式调用
@@ -16,7 +16,13 @@ class MyPromise {
     this.PromiseState = PENDING // 状态
     this.PromiseResult = null // 内部值
 
-    func(this.resolve.bind(this), this.reject.bind(this)) // 立即执行主体
+    try {
+      // 立即执行主体
+      // 主体函数执行错误，会出发 reject
+      func(this.resolve.bind(this), this.reject.bind(this))
+    } catch (error) {
+      this.reject(error)
+    }
   }
 
   resolve(result) {
@@ -32,8 +38,49 @@ class MyPromise {
       this.PromiseResult = reason
     }
   }
+
+  then(onFulfilled, onRejected) {
+    // 参数校验，如果某个参数为空应该默认是忽略
+    onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : value => value
+    onRejected = typeof onRejected === 'function' ? onRejected : reason => { throw reason }
+    if (this.PromiseState === FULFILLED) {
+      onFulfilled(this.PromiseResult)
+    }
+    if (this.PromiseState === REJECTED) {
+      onRejected(this.PromiseResult)
+    }
+  }
 }
 
 let p0 = new MyPromise((resolve, reject) => {
   resolve('ok') // 运行时确定this，这里this = undifined，需要在promise内部绑定 this
 })
+console.log("====================")
+let p1 = new MyPromise((resolve, reject) => {
+  resolve(1)
+})
+p1.then(res => {
+  console.log('p1.res ', res)
+})
+p1 = new MyPromise((resolve, reject) => {
+  reject('error1')
+})
+p1.then(null, error => {
+  console.log('p1.error ', error)
+})
+console.log("====================")
+let p2 = new MyPromise((resolve, reject) => {
+  throw new Error('白嫖失败')
+})
+p2.then(
+  result => console.log('fulfilled: ', result),
+  reason => console.log('rejected: ', reason.message)
+)
+console.log("====================")
+let p3 = new MyPromise((resolve, reject) => {
+  reject('这次一定');
+})
+p3.then(
+  undefined,
+  undefined
+)
